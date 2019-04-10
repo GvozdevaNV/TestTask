fe293cc4d42cdc405ed2e7b4df04f78eb7114ea8
GvozdevaNV <natasha.gwozdewa@yandex.ru>
2019-04-10 06:33:28 +0300

    Второй

--- Task1_Orders/Task1_Orders/Program.cs
+++ Task1_Orders/Task1_Orders/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Data.Entity;
 using Task1_Orders.Models;
+using System.Threading.Tasks;
 
 namespace Task1_Orders
 {
@@ -14,63 +15,85 @@ namespace Task1_Orders
         {
             PrintOrderItemsByOrder();
 
-            ThreadPool.QueueUserWorkItem(UpdateOrderPrimary);
-            ThreadPool.QueueUserWorkItem(UpdateOrderSecondary);
-
-            Thread.Sleep(1000);
+            Task task1 = Task.Factory.StartNew(UpdateOrderPrimary);
+            Task task2 = Task.Factory.StartNew(UpdateOrderSecondary);
+            Task.WaitAll(task1, task2);
             PrintOrderItemsByOrder();
+
             Console.Read();
         }
 
-        private static void UpdateOrderPrimary(object state)
+        private static void UpdateOrderPrimary()
         {
             using (OrderContext db = new OrderContext())
             {
-                Thread.CurrentThread.Name = "Primary";
-                Console.WriteLine("Запущен поток {0}\n", Thread.CurrentThread.Name);
-                var order = db.Orders.Include(x => x.Items).Single(o => o.Id == 1);
-                new OrderDBServices().InsertOrderItem(db, new OrderItem { Title = "Primary", Count = 1, Price = 1.0M, Order = order });
-                db.SaveChanges();
-                Console.WriteLine("Поток {0} завершил работу\n", Thread.CurrentThread.Name);
-                #region test
-                //PrintOrderItems(db);
-                //PrintOrderItemsByOrder(db);
-                //Console.WriteLine(new string('-', 50));
+                using (var tran = db.Database.BeginTransaction(System.Data.IsolationLevel.RepeatableRead))
+                {
+                    try
+                    {
+                        Thread.CurrentThread.Name = "Primary";
+                        Console.WriteLine("Запущен поток {0}\n", Thread.CurrentThread.Name);
+                        var order = db.Orders.Include(x => x.Items).Single(o => o.Id == 1);
+                        new OrderDBServices().InsertOrderItem(db, new OrderItem { Title = "Primary", Count = 1, Price = 1.0M, Order = order });
+                        db.SaveChanges();
+                        Console.WriteLine("Поток {0} завершил работу\n", Thread.CurrentThread.Name);
+                        #region test
+                        //PrintOrderItems(db);
+                        //PrintOrderItemsByOrder(db);
+                        //Console.WriteLine(new string('-', 50));
 
-                //int newCount;
-                //decimal newPrice;
-                //var orderItem = db.OrderItems.Single(oi => oi.Id == 6);
-                //newCount = 3;
-                //dbServices.UpdateOrderItemCountOrPrice(db, orderItem, newCount, null);
-                //newCount = 1;
-                //dbServices.UpdateOrderItemCountOrPrice(db, orderItem, newCount, null);
-                //newPrice = 100.0M;
-                //dbServices.UpdateOrderItemCountOrPrice(db, orderItem, null, newPrice);
-                //newPrice = 50.0M;
-                //dbServices.UpdateOrderItemCountOrPrice(db, orderItem, null, newPrice);
-                //newCount = 1;
-                //newPrice = 45.5M;
-                //dbServices.UpdateOrderItemCountOrPrice(db, orderItem, newCount, newPrice);
-                //dbServices.UpdateOrderItemCountOrPrice(db, orderItem, null, null);
-                //dbServices.DeleteOrderItem(db, orderItem);
-                //db.SaveChanges();
+                        //int newCount;
+                        //decimal newPrice;
+                        //var orderItem = db.OrderItems.Single(oi => oi.Id == 6);
+                        //newCount = 3;
+                        //dbServices.UpdateOrderItemCountOrPrice(db, orderItem, newCount, null);
+                        //newCount = 1;
+                        //dbServices.UpdateOrderItemCountOrPrice(db, orderItem, newCount, null);
+                        //newPrice = 100.0M;
+                        //dbServices.UpdateOrderItemCountOrPrice(db, orderItem, null, newPrice);
+                        //newPrice = 50.0M;
+                        //dbServices.UpdateOrderItemCountOrPrice(db, orderItem, null, newPrice);
+                        //newCount = 1;
+                        //newPrice = 45.5M;
+                        //dbServices.UpdateOrderItemCountOrPrice(db, orderItem, newCount, newPrice);
+                        //dbServices.UpdateOrderItemCountOrPrice(db, orderItem, null, null);
+                        //dbServices.DeleteOrderItem(db, orderItem);
+                        //db.SaveChanges();
 
-                //PrintOrderItemsByOrder();
-                //Console.WriteLine(new string('-', 50));
-                #endregion
+                        //PrintOrderItemsByOrder();
+                        //Console.WriteLine(new string('-', 50));
+                        #endregion
+                        tran.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        tran.Rollback();
+                    }
+                }
             }
         }
 
-        private static void UpdateOrderSecondary(object state)
+        private static void UpdateOrderSecondary()
         {
             using (OrderContext db = new OrderContext())
             {
+                //using (var tran = db.Database.BeginTransaction(System.Data.IsolationLevel.RepeatableRead))
+                //{
+                //    try
+                //    {
                 Thread.CurrentThread.Name = "Secondary";
                 Console.WriteLine("Запущен поток {0}\n", Thread.CurrentThread.Name);
                 var order = db.Orders.Include(x => x.Items).Single(o => o.Id == 1);
                 new OrderDBServices().InsertOrderItem(db, new OrderItem { Title = "Secondary", Count = 1, Price = 1.0M, Order = order });
                 db.SaveChanges();
-                Console.WriteLine("Поток {0} завершил работу\n", Thread.CurrentThread.Name);            
+                Console.WriteLine("Поток {0} завершил работу\n", Thread.CurrentThread.Name);
+                //        tran.Commit();
+                //    }
+                //    catch(Exception ex)
+                //    {
+                //        tran.Rollback();
+                //    }
+                //}           
             }
         }
 
